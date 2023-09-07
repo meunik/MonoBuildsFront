5d3ad2e939e36f628e9bc7a6c959b2aae096ef0e
meunik <meunik@live.com>
2023-09-07 12:13:44 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/MonoBuildsFront/',
-  build: {
-    outDir: 'docs',
-  },
   plugins: [vue()],
   resolve: {
     alias: {
