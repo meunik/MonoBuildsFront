85117a9070bbd0b01a85dfe7dbf4b417021508eb
meunik <meunik@live.com>
2023-09-13 21:40:39 -0300

    ajustes no package

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production' ? '/MonoBuildsFront/' : '/',
-  build: { outDir: 'docs' },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
